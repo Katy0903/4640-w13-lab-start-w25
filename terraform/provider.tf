969fc481aa0028ce2c7f4a0d5091e60ee84c3de7
Katy_wsl2_ubuntu <cchan532@my.bcit.ca>
2025-04-01 15:23:47 -0700

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,23 @@
 terraform {
+  required_version = ">= 1.10.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "myyybucketbcit"   
+    key            = "terraform.tfstate"               
+    region         = "us-west-2"                    
+    encrypt        = true                            
+    use_lockfile   = true                      
+  }
+
 }
 
 provider "aws" {
   region = "us-west-2"
 }
+
